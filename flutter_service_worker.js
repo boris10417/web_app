0f3484f67cc635223990bd9198fd0839c733fd02
boris <boris10417@gmail.com>
2023-07-13 17:59:00 +0800

    新增:
    1.目前可以在本地儲存，至於儲存在哪，不知道，總而言之不需要再做資料庫的東西了，每個裝置都是只能使用本地資料

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -23,9 +23,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "d335e9891b264c173aa6071fbc625ebd",
-"/": "d335e9891b264c173aa6071fbc625ebd",
-"main.dart.js": "7f43050e75f98589d655ee40860e7a38",
+"index.html": "15bb07df82318ebf85d549a5f992b9cd",
+"/": "15bb07df82318ebf85d549a5f992b9cd",
+"main.dart.js": "fe25c77fc1ac29bea9458c9667a0f97e",
 "manifest.json": "3f9f46d43b9a25da68f5dd4800d5ed6d",
 "version.json": "c2a08cda2a1e690bfed5ce9fc4d9e5b1"};
 // The application shell files that are downloaded before a service worker can
