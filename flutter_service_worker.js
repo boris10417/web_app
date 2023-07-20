c82294bb4d225c2bcef60d8acde462fc6763391e
boris <boris10417@gmail.com>
2023-07-20 15:50:14 +0800

    修改:
    1.便當頁面，改成可以儲存的，且可以從開始日期累計至今天，但沒有防呆所以要小心使用。

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -23,9 +23,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "d9cbf02f5324b6595da0b6e167d143fb",
-"/": "d9cbf02f5324b6595da0b6e167d143fb",
-"main.dart.js": "b0df682ae3d8ce607817c7d0f0a60b19",
+"index.html": "6f4f2623686a533b60aa6934ed91f599",
+"/": "6f4f2623686a533b60aa6934ed91f599",
+"main.dart.js": "7e3c2126f8e9b49de7fd39795ed66c23",
 "manifest.json": "3f9f46d43b9a25da68f5dd4800d5ed6d",
 "version.json": "c2a08cda2a1e690bfed5ce9fc4d9e5b1"};
 // The application shell files that are downloaded before a service worker can
