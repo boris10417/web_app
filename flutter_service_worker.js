20cf86a1c82b3bba966c858e154e8eb6e116bad6
boris <boris10417@gmail.com>
2023-07-26 11:29:57 +0800

    測試相對路徑的build是否能成功

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -23,8 +23,8 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "45ddf8bbac34c85b1e43e13fee356511",
-"/": "45ddf8bbac34c85b1e43e13fee356511",
+"index.html": "a46b979cdbaf39cf04a5416abfc1c1e7",
+"/": "a46b979cdbaf39cf04a5416abfc1c1e7",
 "main.dart.js": "3f3ac600c630e3114daaa7c411e9b424",
 "manifest.json": "3f9f46d43b9a25da68f5dd4800d5ed6d",
 "version.json": "c2a08cda2a1e690bfed5ce9fc4d9e5b1"};
