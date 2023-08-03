b8091abcbd4afcc1021c0a6c019cbc5d6d897ae0
boris <boris10417@gmail.com>
2023-08-03 13:21:10 +0800

    再次嘗試image picker是否能在網頁上執行

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -24,9 +24,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "37d371dfb229bdb7d748a01d60e738bf
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "c997161b7f1e9b68592280155d455be4",
-"/": "c997161b7f1e9b68592280155d455be4",
-"main.dart.js": "d35da7d53825cb86c6faf2a7d73aa549",
+"index.html": "e8ba2094623fdade8d88e1e4266b0636",
+"/": "e8ba2094623fdade8d88e1e4266b0636",
+"main.dart.js": "5bd6ea7b0cfe5c333a516565f916e284",
 "manifest.json": "3f9f46d43b9a25da68f5dd4800d5ed6d",
 "version.json": "c2a08cda2a1e690bfed5ce9fc4d9e5b1"};
 // The application shell files that are downloaded before a service worker can
